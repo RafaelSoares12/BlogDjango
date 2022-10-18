768819f9ed9bc9c1ae362e2fdf1a5ae5bd4ef0a3
soares.lopes <soares.lopes@mail.uft.edu.br>
2022-10-18 10:02:34 -0300

    resolvendo problemas de diretorios

---
+++ myblog/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from .views import HomeView, ArticleDetailView 
+
+urlpatterns = [
+    path('', HomeView.as_view(), name="index"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail")
+]
